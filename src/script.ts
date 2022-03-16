3e22a3f6a7ef64522076001a5e397ed11a26e905
Alexander Marks <aomarks@gmail.com>
2022-03-16 14:29:22 -0700

    Add events, errors, and loggers (#19)
    Event
    ----------
    Adds an Event type, which contains structured information about events that can happen during the course of Wireit's execution. This includes Success, Failure, Output, and Info events. The purpose is to separate semantic logging from presentation, so that we can implement different approaches for handling errors, script output, and other info that the user may care about.
    
    For example:
    
    {
      type: 'success',
      reason: 'exit-zero',
      script: {
        package: '/some/package/dir',
        script: 'build',
      }
    }
    
    {
      type: 'failure',
      reason: 'exit-non-zero',
      status: 42,
      script: {
        package: '/some/package/dir',
        script: 'build',
      }
    }
    
    {
      type: 'output',
      stream: 'stdout',
      data: 'some output from the build script',
      script: {
        package: '/some/package/dir',
        script: 'build',
      }
    }
    
    Logger
    ----------
    Adds a Logger interface, and a DefaultLogger class. Loggers receive events and display events to the user in some way. The DefaultLogger contains custom formatted messages for each kind of Event and writes them to stdout/stderr. Other Loggers we could add in the future could be things like a tmux one, one that logs to files, etc.
    
    WireitError
    ----------
    The WireitError class is a subclass of Error which has an event property which must be a Failure event. If a WireitError is thrown during execution, then its event will ultimately get passed to the logger. This lets us use exceptions for control flow, but still standardize on the Event type for representing all errors.

---
+++ src/script.ts
@@ -0,0 +1,48 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * The location on disk of an npm package.
+ */
+export interface PackageReference {
+  /** Absolute path to an npm package directory. */
+  package: string;
+}
+
+/**
+ * The name and package location of a script.
+ */
+export interface ScriptReference extends PackageReference {
+  /** A concrete script name (no ./ or $WORKSPACES etc.) */
+  script: string;
+}
+
+/**
+ * The name and package location of a script, along with its parents.
+ */
+export interface ScriptReferenceWithParents extends ScriptReference {
+  /**
+   * The script(s) in the dependency graph that are inbound to this one. Helpful
+   * for understanding the path(s) taken to reach a script.
+   */
+  parents: Script[];
+}
+
+/**
+ * The name and location of a script, along with its parents, and its full
+ * configuration.
+ */
+export interface Script extends ScriptReferenceWithParents {
+  /**
+   * The shell command to execute.
+   *
+   * An undefined command is valid as a way to give name to a group of other
+   * scripts (specified as dependencies).
+   */
+  command?: string;
+  /** Scripts that must run before this one. */
+  dependencies: Script[];
+}
