550cb55d8d8932a5b8a049cf7abe7ae27a82eb1a
Alexander Marks <aomarks@google.com>
2022-03-27 17:06:32 -0700

    Add workarounds for silent before/after errors
    See https://github.com/lukeed/uvu/issues/191

--- src/test/errors-analysis.test.ts
+++ src/test/errors-analysis.test.ts
@@ -13,12 +13,26 @@ import {WireitTestRig} from './util/test-rig.js';
 const test = suite<{rig: WireitTestRig}>();
 
 test.before.each(async (ctx) => {
-  ctx.rig = new WireitTestRig();
-  await ctx.rig.setup();
+  try {
+    ctx.rig = new WireitTestRig();
+    await ctx.rig.setup();
+  } catch (error) {
+    // Uvu has a bug where it silently ignores failures in before and after,
+    // see https://github.com/lukeed/uvu/issues/191.
+    console.error('uvu before error', error);
+    process.exit(1);
+  }
 });
 
 test.after.each(async (ctx) => {
-  await ctx.rig.cleanup();
+  try {
+    await ctx.rig.cleanup();
+  } catch (error) {
+    // Uvu has a bug where it silently ignores failures in before and after,
+    // see https://github.com/lukeed/uvu/issues/191.
+    console.error('uvu after error', error);
+    process.exit(1);
+  }
 });
 
 test(
