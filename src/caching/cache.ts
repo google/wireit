2da1d3cd31053b96ea8d33172ef9f2a636c9f063
Alexander Marks <aomarks@google.com>
2022-03-30 16:08:09 -0700

    Add Cache interface

---
+++ src/caching/cache.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import type {CacheKeyString, ScriptReference} from '../script.js';
+
+/**
+ * Saves and restores output files to some cache store (e.g. local disk or
+ * remote server).
+ */
+export interface Cache {
+  /**
+   * Check for a cache hit for the given script and cache key. Don't write it to
+   * disk yet, instead return a {@link CacheHit} which can be used to control
+   * when writing occurs.
+   *
+   * @param script The script whose output will be read from the cache.
+   * @param cacheKey The string-encoded cache key for the script.
+   * @return Promise of a {@link CacheHit} if there was an entry in the cache,
+   * or undefined if there was not.
+   */
+  get(
+    script: ScriptReference,
+    cacheKey: CacheKeyString
+  ): Promise<CacheHit | undefined>;
+
+  /**
+   * Write the given file paths to the cache, keyed by the given script and
+   * cache key.
+   *
+   * @param script The script whose output will be saved to the cache.
+   * @param cacheKey The string-encoded cache key for the script.
+   * @param relativeFilePaths The package-relative output file paths to cache
+   * (concrete paths, not glob patterns).
+   */
+  set(
+    script: ScriptReference,
+    cacheKey: CacheKeyString,
+    relativeFilePaths: string[]
+  ): Promise<void>;
+}
+
+/**
+ * The result of {@link Cache.get}.
+ *
+ * Note the reason {@link Cache.get} reteurns this class instead of immediately
+ * applying writing the cached output is so that the {@link Executor} can
+ * control the timing of when cached output is written.
+ */
+export interface CacheHit {
+  /**
+   * Write the cached files to disk.
+   *
+   * It is assumed that any existing stale output has already been cleaned
+   * before this method is called.
+   */
+  apply(): Promise<void>;
+}
