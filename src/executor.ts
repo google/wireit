65623c0c127d3dfbc92b5fc37bc869875ed4d3fd
Alexander Marks <aomarks@google.com>
2022-04-06 15:57:30 -0700

    Generate a PATH environment variable for each script

--- src/executor.ts
+++ src/executor.ts
@@ -35,6 +35,28 @@ import type {Cache} from './caching/cache.js';
  */
 const UNCACHEABLE = Symbol();
 
+/**
+ * The PATH environment variable of this process, minus all of the leading
+ * "node_modules/.bin" entries that the incoming "npm run" command already set.
+ *
+ * We want full control over which "node_modules/.bin" paths are in the PATH of
+ * the processes we spawn, so that cross-package dependencies act as though we
+ * are running "npm run" with each package as the cwd.
+ *
+ * We only need to do this once per Wireit process, because process.env never
+ * changes.
+ */
+const PATH_ENV_SUFFIX = (() => {
+  const path = process.env.PATH ?? '';
+  // Note the PATH delimiter is platform-dependent.
+  const entries = path.split(pathlib.delimiter);
+  const nodeModulesBinSuffix = pathlib.join('node_modules', '.bin');
+  const endOfNodeModuleBins = entries.findIndex(
+    (entry) => !entry.endsWith(nodeModulesBinSuffix)
+  );
+  return entries.slice(endOfNodeModuleBins).join(pathlib.delimiter);
+})();
+
 const IS_WINDOWS = process.platform === 'win32';
 
 /**
@@ -254,9 +276,8 @@ class ScriptExecution {
 
     const command = this.#script.command;
     await this.#workerPool.run(async () => {
-      // TODO(aomarks) Fix PATH and npm_ environment variables to reflect the new
-      // package when cross-package dependencies are supported.
-
+      // TODO(aomarks) Update npm_ environment variables to reflect the new
+      // package.
       const child = spawn(command, {
         cwd: this.#script.packageDir,
         // Conveniently, "shell:true" has the same shell-selection behavior as
@@ -268,6 +289,10 @@ class ScriptExecution {
         //   https://nodejs.org/api/child_process.html#default-windows-shell
         //   https://github.com/npm/run-script/blob/a5b03bdfc3a499bf7587d7414d5ea712888bfe93/lib/make-spawn-args.js#L11
         shell: true,
+        env: {
+          ...process.env,
+          PATH: this.#pathEnvironmentVariable,
+        },
       });
 
       // Only create the stdout/stderr replay files if we encounter anything on
@@ -359,6 +384,29 @@ class ScriptExecution {
       type: 'success',
       reason: 'exit-zero',
     });
+  }
+  /**
+   * Generates the PATH environment variable that should be set when this
+   * script's command is spawned.
+   */
+  get #pathEnvironmentVariable(): string {
+    // Given package "/foo/bar", walk up the path hierarchy to generate
+    // "/foo/bar/node_modules/.bin:/foo/node_modules/.bin:/node_modules/.bin".
+    const entries = [];
+    let cur = this.#script.packageDir;
+    while (true) {
+      entries.push(pathlib.join(cur, 'node_modules', '.bin'));
+      const parent = pathlib.dirname(cur);
+      if (parent === cur) {
+        break;
+      }
+      cur = parent;
+    }
+    // Add the inherited PATH variable, minus any "node_modules/.bin" entries
+    // that were set by the "npm run" command that spawned Wireit.
+    entries.push(PATH_ENV_SUFFIX);
+    // Note the PATH delimiter is platform-dependent.
+    return entries.join(pathlib.delimiter);
   }
 
   /**
