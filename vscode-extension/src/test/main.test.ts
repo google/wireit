4e255321f63050ed18fc6f735112b1a789e3d6a6
rictic <rictic@gmail.com>
2022-05-03 15:59:07 -0500

    Update line numbers in test to correspond with changed fixture.

--- vscode-extension/src/test/main.test.ts
+++ vscode-extension/src/test/main.test.ts
@@ -90,8 +90,8 @@ test('warns on a package.json based on semantic analysis in the language server'
     })),
     [
       {start: {line: 2, character: 26}, end: {line: 2, character: 31}},
-      {start: {line: 10, character: 4}, end: {line: 10, character: 20}},
-      {start: {line: 16, character: 4}, end: {line: 16, character: 38}},
+      {start: {line: 11, character: 4}, end: {line: 11, character: 20}},
+      {start: {line: 17, character: 4}, end: {line: 17, character: 38}},
     ],
     JSON.stringify(
       diagnostics.map((d) => ({
