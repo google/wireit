74e21a37aa5c3e8791da36a27b4dc3efa2485e7b
Peter Burns <rictic@google.com>
2022-05-03 20:23:18 -0500

    Add missing license header.

--- src/test/util/colors.ts
+++ src/test/util/colors.ts
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export function removeAciiColors(s: string): string {
   // eslint-disable-next-line no-control-regex
   return s.replace(/\x1b\[\d+m/g, '');
