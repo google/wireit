019bfe70083bc840c3028db7f119b3610185e7c1
Alexander Marks <aomarks@google.com>
2023-02-05 15:55:26 -0800

    Add WatchLogger

--- src/watcher.ts
+++ src/watcher.ts
@@ -11,6 +11,7 @@ import {Executor, FailureMode, ServiceMap} from './executor.js';
 import {Logger} from './logging/logger.js';
 import {Deferred} from './util/deferred.js';
 import {WorkerPool} from './util/worker-pool.js';
+import {WatchLogger} from './logging/watch-logger.js';
 import {
   ScriptConfig,
   ScriptReference,
@@ -137,7 +138,7 @@ export class Watcher {
   ) {
     this._rootScript = rootScript;
     this._extraArgs = extraArgs;
-    this._logger = logger;
+    this._logger = new WatchLogger(logger);
     this._workerPool = workerPool;
     this._failureMode = failureMode;
     this._cache = cache;
