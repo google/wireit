0d3de2e94719309b5e3913c60f4ca5aeb6bbf93a
Peter Burns <rictic@google.com>
2023-09-01 09:10:12 -0700

    npm run format

--- src/logging/quiet/writeover-line.ts
+++ src/logging/quiet/writeover-line.ts
@@ -8,8 +8,10 @@ import {DEBUG} from '../logger.js';
 
 // Quick Symbol.dispose polyfill.
 if (!Symbol.dispose) {
-  type Writeable<T> = { -readonly [P in keyof T]: T[P] };
-  (Symbol as Writeable<typeof Symbol>).dispose = Symbol('dispose') as typeof Symbol.dispose;
+  type Writeable<T> = {-readonly [P in keyof T]: T[P]};
+  (Symbol as Writeable<typeof Symbol>).dispose = Symbol(
+    'dispose',
+  ) as typeof Symbol.dispose;
 }
 
 /**
