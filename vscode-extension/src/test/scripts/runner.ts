97be4517077bf1f5b12487955d9315ea978c8b1e
rictic <rictic@gmail.com>
2023-09-30 15:32:32 -0700

    Downloading vscode for the tests is flaky, so retry a couple times.

--- vscode-extension/src/test/scripts/runner.ts
+++ vscode-extension/src/test/scripts/runner.ts
@@ -8,13 +8,33 @@ import * as path from 'path';
 
 import {runTests} from '@vscode/test-electron';
 
+const MAX_TRIES = 3;
+
+/**
+ * Runs the tests via `run` up to 3 times, because downloading vscode is
+ * flaky.
+ */
+async function main() {
+  for (let i = 0; i < MAX_TRIES - 1; i++) {
+    try {
+      await run();
+      return;
+    } catch (err) {
+      console.error('Failed to run tests, retrying...');
+    }
+    // wait a few seconds before retrying
+    await new Promise((resolve) => setTimeout(resolve, 5_000));
+  }
+  await run();
+}
+
 /**
  * Downloads vscode and starts it in extension test mode, pointing it at
  * ./uvu-entrypoint
  *
  * Note that uvu-entrypoint runs in its own process, inside of electron.
  */
-async function main() {
+async function run() {
   const extensionDevelopmentPath = path.resolve(__dirname, '../../../built');
   const extensionTestsPath = path.resolve(__dirname, './uvu-entrypoint.js');
   await runTests({extensionDevelopmentPath, extensionTestsPath});
