9ad89908904bf515140972bbaca4e5fe28530ece
Alexander Marks <aomarks@gmail.com>
2024-09-07 10:42:52 -0700

    Upgrade eslint and convert to new config file format

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import pluginJs from '@eslint/js';
+import noOnlyTests from 'eslint-plugin-no-only-tests';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/*.js',
+      '**/*.cjs',
+      '**/*.mjs',
+      '**/*.d.ts',
+      '**/.wireit/',
+      '**/node_modules/',
+      'lib/',
+      'vscode-extension/.vscode-test/',
+      'vscode-extension/lib/',
+      'vscode-extension/built/',
+    ],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json', './vscode-extension/tsconfig.json'],
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'no-only-tests': noOnlyTests,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    rules: {
+      'no-only-tests/no-only-tests': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {argsIgnorePattern: '^_', varsIgnorePattern: '^_'},
+      ],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-useless-constructor': 'off',
+      '@typescript-eslint/only-throw-error': 'off',
+      '@typescript-eslint/no-confusing-void-expression': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/no-unnecessary-type-arguments': 'off',
+      '@typescript-eslint/no-unnecessary-template-expression': 'off',
+      '@typescript-eslint/use-unknown-in-catch-callback-variable': 'off',
+      '@typescript-eslint/return-await': 'off',
+    },
+  },
+);
