6a61e89086ef02c12773ee77a08c4e39b88cbc67
Al Marks <aomarks@gmail.com>
2025-02-22 17:33:35 -0800

    Add test for dependency exclusion

--- src/test/workspaces.test.ts
+++ src/test/workspaces.test.ts
@@ -334,6 +334,101 @@ test(
 );
 
 test(
+  '<workspaces>#build with script !exclusions',
+  rigTest(async ({rig}) => {
+    const foo = await rig.newCommand();
+    const bar = await rig.newCommand();
+    const baz = await rig.newCommand();
+    await rig.write({
+      'package.json': {
+        workspaces: ['packages/*'],
+        scripts: {
+          build: 'wireit',
+        },
+        wireit: {
+          build: {
+            dependencies: [
+              // Remove "foo"; no-op since no positive match yet.
+              '!./packages/foo#build',
+              // Add all packages.
+              '<workspaces>#build',
+              // Exclude "bar" and "baz"
+              '!./packages/bar#build',
+              '!./packages/baz#build',
+              // Add back" bar".
+              './packages/bar#build',
+              // ... so we expect "foo" and "bar" to run, but not "baz".
+            ],
+          },
+        },
+      },
+      'packages/foo/package.json': {
+        scripts: {
+          build: 'wireit',
+        },
+        wireit: {
+          build: {
+            command: foo.command,
+            files: [],
+            output: [],
+          },
+        },
+      },
+      'packages/bar/package.json': {
+        scripts: {
+          build: 'wireit',
+        },
+        wireit: {
+          build: {
+            command: bar.command,
+            files: [],
+            output: [],
+          },
+        },
+      },
+      'packages/baz/package.json': {
+        scripts: {
+          build: 'wireit',
+        },
+        wireit: {
+          build: {
+            command: baz.command,
+            files: [],
+            output: [],
+          },
+        },
+      },
+    });
+
+    const process = rig.exec('npm run build');
+    const nextFoo = foo.nextInvocation();
+    const nextBar = bar.nextInvocation();
+
+    const maybeEarlyExit = await Promise.race([
+      process.exit,
+      Promise.all([nextFoo, nextBar]),
+    ]);
+    if ('code' in maybeEarlyExit) {
+      throw new Error(`Unexpected exit: ${maybeEarlyExit.code}`);
+    }
+
+    (await nextFoo).exit(0);
+    (await nextBar).exit(0);
+
+    // Fail quickly if baz starts to run (otherwise we time-out).
+    await new Promise((resolve) => setTimeout(resolve, 50));
+    void baz.nextInvocation().then(() => {
+      throw new Error('Unexpected invocation of baz');
+    });
+
+    assert.equal((await process.exit).code, 0);
+    assert.equal(foo.numInvocations, 1);
+    assert.equal(bar.numInvocations, 1);
+    assert.equal(baz.numInvocations, 0);
+  }),
+);
+
+test(
   '<workspaces>:build --watch',
   rigTest(async ({rig}) => {
     const foo = await rig.newCommand();
