17294dc192ce43b4fa42acde099f78bcdcf2cb64
Al Marks <aomarks@gmail.com>
2026-02-09 20:19:45 -0800

    Migrate copy.test.ts from uvu to node:test

--- src/test/copy.test.ts
+++ src/test/copy.test.ts
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {suite} from 'uvu';
-import * as assert from 'uvu/assert';
+import {test} from 'node:test';
+import * as assert from 'node:assert';
 import {FilesystemTestRig} from './util/filesystem-test-rig.js';
 import * as pathlib from 'path';
 import * as fs from 'fs/promises';
@@ -15,98 +15,101 @@ import {copyEntries} from '../util/copy.js';
 
 import type {AbsoluteEntry} from '../util/glob.js';
 
-const test = suite<{
-  src: FilesystemTestRig;
-  dst: FilesystemTestRig;
+async function setup(): Promise<
+  {
+    src: FilesystemTestRig;
+    dst: FilesystemTestRig;
 
-  /** Make a fake glob AbsoluteEntry that looks like a regular file. */
-  file: (path: string) => AbsoluteEntry;
+    /** Make a fake glob AbsoluteEntry that looks like a regular file. */
+    file: (path: string) => AbsoluteEntry;
 
-  /** Make a fake glob AbsoluteEntry that looks like a directory. */
-  dir: (path: string) => AbsoluteEntry;
+    /** Make a fake glob AbsoluteEntry that looks like a directory. */
+    dir: (path: string) => AbsoluteEntry;
 
-  /** Make a fake glob AbsoluteEntry that looks like a symbolic link. */
-  symlink: (path: string) => AbsoluteEntry;
-}>();
+    /** Make a fake glob AbsoluteEntry that looks like a symbolic link. */
+    symlink: (path: string) => AbsoluteEntry;
+  } & AsyncDisposable
+> {
+  const src = new FilesystemTestRig();
+  const dst = new FilesystemTestRig();
+  await src.setup();
+  await dst.setup();
 
-test.before.each(async (ctx) => {
-  try {
-    ctx.src = new FilesystemTestRig();
-    ctx.dst = new FilesystemTestRig();
-    await ctx.src.setup();
-    await ctx.dst.setup();
+  const file = (path: string) =>
+    ({
+      path: src.resolve(windowsifyPathIfOnWindows(path)),
+      dirent: {
+        isFile: () => true,
+        isDirectory: () => false,
+        isSymbolicLink: () => false,
+      },
+    }) as AbsoluteEntry;
 
-    ctx.file = (path) =>
-      ({
-        path: ctx.src.resolve(windowsifyPathIfOnWindows(path)),
-        dirent: {
-          isFile: () => true,
-          isDirectory: () => false,
-          isSymbolicLink: () => false,
-        },
-      }) as AbsoluteEntry;
+  const dir = (path: string) =>
+    ({
+      path: src.resolve(windowsifyPathIfOnWindows(path)),
+      dirent: {
+        isFile: () => false,
+        isDirectory: () => true,
+        isSymbolicLink: () => false,
+      },
+    }) as AbsoluteEntry;
 
-    ctx.dir = (path) =>
-      ({
-        path: ctx.src.resolve(windowsifyPathIfOnWindows(path)),
-        dirent: {
-          isFile: () => false,
-          isDirectory: () => true,
-          isSymbolicLink: () => false,
-        },
-      }) as AbsoluteEntry;
+  const symlink = (path: string) =>
+    ({
+      path: src.resolve(windowsifyPathIfOnWindows(path)),
+      dirent: {
+        isFile: () => false,
+        isDirectory: () => false,
+        isSymbolicLink: () => true,
+      },
+    }) as AbsoluteEntry;
 
-    ctx.symlink = (path) =>
-      ({
-        path: ctx.src.resolve(windowsifyPathIfOnWindows(path)),
-        dirent: {
-          isFile: () => false,
-          isDirectory: () => false,
-          isSymbolicLink: () => true,
-        },
-      }) as AbsoluteEntry;
-  } catch (error) {
-    // Uvu has a bug where it silently ignores failures in before and after,
-    // see https://github.com/lukeed/uvu/issues/191.
-    console.error('uvu before error', error);
-    process.exit(1);
-  }
-});
+  return {
+    src,
+    dst,
+    file,
+    dir,
+    symlink,
+    async [Symbol.asyncDispose]() {
+      await src.cleanup();
+      await dst.cleanup();
+    },
+  };
+}
 
-test.after.each(async (ctx) => {
-  try {
-    await ctx.src.cleanup();
-    await ctx.dst.cleanup();
-  } catch (error) {
-    // Uvu has a bug where it silently ignores failures in before and after,
-    // see https://github.com/lukeed/uvu/issues/191.
-    console.error('uvu after error', error);
-    process.exit(1);
-  }
-});
-
-test('ignore empty entries', async ({src, dst}) => {
+void test('ignore empty entries', async () => {
+  await using context = await setup();
+  const {src, dst} = context;
   await copyEntries([], src.temp, dst.temp);
 });
 
-test('copy file', async ({src, dst, file}) => {
+void test('copy file', async () => {
+  await using context = await setup();
+  const {src, dst, file} = context;
   await src.write('foo', 'content');
   await copyEntries([file('foo')], src.temp, dst.temp);
-  assert.equal(await dst.read('foo'), 'content');
+  assert.deepStrictEqual(await dst.read('foo'), 'content');
 });
 
-test('ignore non-existent file', async ({src, dst, file}) => {
+void test('ignore non-existent file', async () => {
+  await using context = await setup();
+  const {src, dst, file} = context;
   await copyEntries([file('foo')], src.temp, dst.temp);
-  assert.not(await dst.exists('foo'));
+  assert.ok(!(await dst.exists('foo')));
 });
 
-test('make empty directory', async ({src, dst, dir}) => {
+void test('make empty directory', async () => {
+  await using context = await setup();
+  const {src, dst, dir} = context;
   await src.mkdir('foo');
   await copyEntries([dir('foo')], src.temp, dst.temp);
   assert.ok(await dst.isDirectory('foo'));
 });
 
-test('make non-existent directory', async ({src, dst, dir}) => {
+void test('make non-existent directory', async () => {
+  await using context = await setup();
+  const {src, dst, dir} = context;
   // We don't actually know if a directory really exists or not, so we just
   // create it regardless. We'd have to stat() to find out; better to just trust
   // the glob results being passed in.
@@ -114,50 +117,41 @@ test('make non-existent directory', async ({src, dst, dir}) => {
   assert.ok(await dst.isDirectory('foo'));
 });
 
-test('copy listed directory with listed child', async ({
-  src,
-  dst,
-  file,
-  dir,
-}) => {
+void test('copy listed directory with listed child', async () => {
+  await using context = await setup();
+  const {src, dst, file, dir} = context;
   await src.mkdir('foo');
   await src.write('foo/bar', 'content');
   await copyEntries([file('foo/bar'), dir('foo')], src.temp, dst.temp);
   assert.ok(await dst.isDirectory('foo'));
-  assert.equal(await dst.read('foo/bar'), 'content');
+  assert.deepStrictEqual(await dst.read('foo/bar'), 'content');
 });
 
-test('copy listed directory but not its unlisted child', async ({
-  src,
-  dst,
-  dir,
-}) => {
+void test('copy listed directory but not its unlisted child', async () => {
+  await using context = await setup();
+  const {src, dst, dir} = context;
   await src.mkdir('foo');
   await src.write('foo/bar', 'content');
   await copyEntries([dir('foo')], src.temp, dst.temp);
   assert.ok(await dst.isDirectory('foo'));
-  assert.not(await dst.exists('foo/bar'));
+  assert.ok(!(await dst.exists('foo/bar')));
 });
 
-test('automatically create parent directory of file', async ({
-  src,
-  dst,
-  file,
-}) => {
+void test('automatically create parent directory of file', async () => {
+  await using context = await setup();
+  const {src, dst, file} = context;
   // We don't require the parent to be listed explicitly, we create them
   // automatically.
   await src.mkdir('foo');
   await src.write('foo/bar', 'content');
   await copyEntries([file('foo/bar')], src.temp, dst.temp);
   assert.ok(await dst.isDirectory('foo'));
-  assert.equal(await dst.read('foo/bar'), 'content');
+  assert.deepStrictEqual(await dst.read('foo/bar'), 'content');
 });
 
-test('automatically create parent directory of directory', async ({
-  src,
-  dst,
-  dir,
-}) => {
+void test('automatically create parent directory of directory', async () => {
+  await using context = await setup();
+  const {src, dst, dir} = context;
   // We don't require the parent to be listed explicitly, we create them
   // automatically.
   await src.mkdir('foo/bar');
@@ -166,7 +160,9 @@ test('automatically create parent directory of directory', async ({
   assert.ok(await dst.isDirectory('foo/bar'));
 });
 
-test('file that already exists is error', async ({src, dst, file}) => {
+void test('file that already exists is error', async () => {
+  await using context = await setup();
+  const {src, dst, file} = context;
   // We error if a file already exists in the destination, because that
   // indicates a bug, like writing to the wrong cache directory.
   await src.write('foo', 'new content');
@@ -177,20 +173,24 @@ test('file that already exists is error', async ({src, dst, file}) => {
   } catch (e) {
     error = e;
   }
-  assert.instance(error, Error);
-  assert.equal((error as {code: string}).code, 'EEXIST');
-  assert.equal(await dst.read('foo'), 'old content');
+  assert.ok(error instanceof Error);
+  assert.deepStrictEqual((error as unknown as {code: string}).code, 'EEXIST');
+  assert.deepStrictEqual(await dst.read('foo'), 'old content');
 });
 
-test('file listed twice is not an error', async ({src, dst, file}) => {
+void test('file listed twice is not an error', async () => {
+  await using context = await setup();
+  const {src, dst, file} = context;
   // We error if a file already existed in the destination, but not if the same
   // file was listed twice in the given entries, because we dedupe.
   await src.write('foo', 'content');
   await copyEntries([file('foo'), file('foo')], src.temp, dst.temp);
-  assert.equal(await dst.read('foo'), 'content');
+  assert.deepStrictEqual(await dst.read('foo'), 'content');
 });
 
-test('directory that already exists is not error', async ({src, dst, dir}) => {
+void test('directory that already exists is not error', async () => {
+  await using context = await setup();
+  const {src, dst, dir} = context;
   // It doesn't really matter if a directory already existed in the destination,
   // because one directory with a given name is as good as another. Plus mkdir()
   // doesn't have an option to check, so we'd have to do an extra stat().
@@ -200,40 +200,48 @@ test('directory that already exists is not error', async ({src, dst, dir}) => {
   assert.ok(await dst.isDirectory('foo'));
 });
 
-test('directory listed twice is not an error', async ({src, dst, dir}) => {
+void test('directory listed twice is not an error', async () => {
+  await using context = await setup();
+  const {src, dst, dir} = context;
   await src.mkdir('foo');
   await copyEntries([dir('foo'), dir('foo')], src.temp, dst.temp);
   assert.ok(await dst.isDirectory('foo'));
 });
 
-test('copies symlink to file verbatim', async ({src, dst, symlink}) => {
+void test('copies symlink to file verbatim', async () => {
+  await using context = await setup();
+  const {src, dst, symlink} = context;
   await src.write('target', 'content');
   await src.symlink('target', 'symlink', 'file');
   await copyEntries([symlink('symlink')], src.temp, dst.temp);
-  assert.equal(await dst.readlink('symlink'), 'target');
-  assert.not(await dst.exists('target'));
+  assert.deepStrictEqual(await dst.readlink('symlink'), 'target');
+  assert.ok(!(await dst.exists('target')));
 
   // If we create the target file, we should now be able to read it. This is
   // mostly to confirm we created the right kind of symlink on Windows.
   await dst.write('target', 'content');
-  assert.equal(await dst.read('symlink'), 'content');
+  assert.deepStrictEqual(await dst.read('symlink'), 'content');
 });
 
-test('copies symlink to directory verbatim', async ({src, dst, symlink}) => {
+void test('copies symlink to directory verbatim', async () => {
+  await using context = await setup();
+  const {src, dst, symlink} = context;
   await src.mkdir('target');
   await src.symlink('target', 'symlink', 'dir');
   await copyEntries([symlink('symlink')], src.temp, dst.temp);
-  assert.equal(await dst.readlink('symlink'), 'target');
-  assert.not(await dst.exists('target'));
+  assert.deepStrictEqual(await dst.readlink('symlink'), 'target');
+  assert.ok(!(await dst.exists('target')));
 
   // If we create the target directory, we should now be able to list it. This
   // is mostly to confirm we created the right kind of symlink on Windows.
   await dst.mkdir('target');
   await dst.touch('target/child');
-  assert.equal(await fs.readdir(dst.resolve('symlink')), ['child']);
+  assert.deepStrictEqual(await fs.readdir(dst.resolve('symlink')), ['child']);
 });
 
-test('stress test', async ({src, dst, file, dir}) => {
+void test('stress test', async () => {
+  await using context = await setup();
+  const {src, dst, file, dir} = context;
   const numRoots = 10;
   const depthPerRoot = 10;
   const filesPerDir = 300;
@@ -280,9 +288,7 @@ test('stress test', async ({src, dst, file, dir}) => {
     if (dirent.isDirectory()) {
       assert.ok(dst.isDirectory(path));
     } else {
-      assert.equal(await dst.read(path), `content for ${path}`);
+      assert.deepStrictEqual(await dst.read(path), `content for ${path}`);
     }
   }
 });
-
-test.run();
